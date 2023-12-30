40974f1919e7e177df9e1d91ae66cd1e9815e2ba
Gustavo Zonta <gustavozontadasilva@gmail.com>
2023-12-30 11:59:37 -0300

    Trying out react state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react'
+
 import Header from './components/Header'
 import Button from './components/Button'
 
 import './styles/App.css'
 
+interface Movie {
+  title: string,
+}
+
 function App() {
+
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  function renderNewMovie(newMovie: Movie) {
+    setMovie(newMovie)
+  }
+
+  useEffect(() => {
+    console.log(movie)
+  }, [movie])
+
   return (
     <div className="container">
       <Header />
-      <Button />
+      <Button renderNewMovie={renderNewMovie} />
+      <p>{movie?.title}</p>
       <p>Clique em "Encontrar filme" que traremos informações de algum filme para você assistir hoje.</p>
     </div>
   )
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,48 @@
+import axios from 'axios';
 import { api } from '../../library/api'
 
 import shuffleSvg from '../../assets/shuffle.svg'
 import './style.css'
 
-export default function Button() {
+type Movie = {
+    title: string,
+}
+
+type ButtonProps = {
+    renderNewMovie: (newMovie: Movie) => void,
+}
+
+export default function Button({ renderNewMovie }: ButtonProps) {
 
     async function getMovie() {
         try {
-            const response = await api.get('/11')
-            console.log(response.data);
-            
+            const response = await api.get('/latest')
+            const movieID = response.data.id;
+
+            async function getWorkingMovie() {
+                try {
+                    const randomIndex = Math.floor(Math.random() * movieID)
+                    const { data } = await api.get(`/${randomIndex}`)
+
+                    renderNewMovie(data)
+                    
+
+                } catch (err) {
+                    if (axios.isAxiosError(err)) {
+                        if (err.response?.status.toString()[0] !== "2") {                            
+                            getWorkingMovie()
+                            return
+                        }
+
+                        console.error(err);
+                    } else {
+                        console.error(err);
+                    }
+                }
+            }
+
+            getWorkingMovie()
+
         } catch (err) {
             console.error(err)
         }
